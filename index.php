41183a16819e477ad85cf8f6577314404b0b8a22
arcel martial <kontcheu23@gmail.com>
2024-01-25 14:57:55 +0100

    The commit changes the PHP script to use functions.php instead of loops.php. It removes the do-while loop and replaces it with a for loop. It adds a for loop that iterates over an array of colors and prints each color. (generated by blackbox.ai )

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-require('loops.php');
+require('functions.php');
 ?>
--- loops.php
+++ loops.php
@@ -1,12 +1,24 @@
 <?php
+ //while loop
+// $i = 10;
+// do {
+//     echo "<p>$i</p>";
+//     $i++;
+// }while ($i < 10);
 
-$i = 10;
-do {
-    echo "<p>$i</p>";
-    $i++;
-}while ($i < 10);
+// while($i < 10){
+//     echo "<p>$i</p>";
+//     $i++;
+// }
 
-while($i < 10){
-    echo "<p>$i</p>";
-    $i++;
+//for loops
+
+for( $i = 0 ; $i < 10 ; $i++) {
+    // echo "<p>$i</p>";
+}
+
+$colors = array('red', 'green','blue');
+
+for($i = 0 ; $i < sizeof($colors); $i++){
+    echo '<p>'. $colors[$i] .'</p>';
 }
