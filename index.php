31f55fdc76a719160c8915f6a7e0916a5cc1012d
arcel martial <kontcheu23@gmail.com>
2024-01-25 10:17:34 +0100

    modulus

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-require('arithmeticoperator.php');
+require('modulus.php');
 ?>
---
+++ modulus.php
@@ -0,0 +1,10 @@
+<?php
+$a = 4;
+if($a % 2 == 0){
+    echo "<p> $a is even.</p>";
+}else{
+    echo "<p> $a is odd.</p>";
+}
+
+$a = 15;
+$b = 5;
