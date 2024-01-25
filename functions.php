e57b1884e0cd7053b2ab9adcb3af0884b5f8dd6e
arcel martial <kontcheu23@gmail.com>
2024-01-25 15:21:19 +0100

    Added function to check if a string is a palindrome. (generated by blackbox.ai )

--- functions.php
+++ functions.php
@@ -1,1 +1,14 @@
 <?php 
+function is_palindrome($string) {
+    // convert a string to lowercase and remove spaces
+    $string = str_replace(' ', '',strtolower($string));
+    //chech string against that same string reversed
+    return ( $string == strrev( $string ) );
+}
+
+$check_string = 'woW';
+if(is_palindrome($check_string)){
+    echo "<p> $check_string is a palindrome</p>";
+}else {
+    echo "<p> $check_string is not a palindrome</p>";
+}
