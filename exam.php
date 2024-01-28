6061a8f85e21241dddf18838d1171310a4469f34
arcel martial <kontcheu23@gmail.com>
2024-01-28 16:30:50 +0100

    The commit changes the value of the key 'john' in the array $data from 'salad' to 'yvanw'. It also removes the key 'bob' and its value from the array. (generated by blackbox.ai )

---
+++ exam.php
@@ -0,0 +1,11 @@
+<?php 
+ $score = 89;
+ if($score > 90){
+    var_dump('A');
+ } elseif($score > 80){
+    var_dump('B');
+ } elseif($score > 70){
+    var_dump('C');
+ } else{
+    var_dump('f');
+ }
--- index.php
+++ index.php
@@ -1,8 +1,1 @@
-<?php 
-$data = ['john' =>'salad', 
-'tax'=>['seed','java'],
-'ver'=>'beans',
-'text'=>'vert'];
-
-$data['john'] = 'yvanw';
-var_dump($data['bob']['1']);
+<?php include 'exam.php';
